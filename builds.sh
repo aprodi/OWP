eb03861647b27146812c1600a8aa6157e2cdb365
aprodi <prodi@esrf.fr>
2021-04-04 00:26:16 +0200

    Update builds.sh

--- builds.sh
+++ builds.sh
@@ -2,20 +2,19 @@
 
 mkdir -p builds
 
-# configurations for stages
-## no brim
-openscad -o builds/delta_stage.stl openscad/delta_stage.scad
+# configurations for LED light cap
+## no taper
 openscad -o builds/delta_stage_microscope.stl -D 'transmission_illumination = true' -D 'reflection_illumination = true' openscad/delta_stage.scad
-## smart brim
+## smart taper
 openscad -o builds/delta_stage_smart_brim.stl -D 'brim_radius = 5'  openscad/delta_stage.scad
-openscad -o builds/delta_stage_microscope_smart_brim.stl -D 'brim_radius = 5' -D 'transmission_illumination = true' -D 'reflection_illumination = true' openscad/delta_stage.scad
-
-
 
 # common parts
-openscad -o builds/base_raspi_sangaboard.stl openscad/base_raspi_sangaboard.scad
-openscad -o builds/base.stl openscad/base.scad
-openscad -o builds/feet.stl openscad/feet.scad
+openscad -o builds/owp-base.stl -D 'weight_outer_diameter = 30' -D 'weight_inner_diameter = 25'openscad/owp-base.scad
+openscad -o builds/owp-holder.stl openscad/owp-holder.scad
+openscad -o builds/owp-wing.stl openscad/owp-wing.scad
+openscad -o builds/owp-hex.stl openscad/.scad
+openscad -o builds/owp-ring.stl openscad/owp-ring.scad
 
 #optional parts
-openscad -o builds/35mm_petri_dish_holder.stl openscad/35mm_petri_dish_holder.scad
+openscad -o builds/cutting_jig_pvc_pipe-40mm-v2.stl openscad/cutting_jig_pvc_pipe-40mm-v2.scad
+openscad -o builds/cutting_jig_pvc_pipe-40mm-v3.stl openscad/cutting_jig_pvc_pipe-40mm-v3.scad
